45de141bb1642c27b2df9a8782b9ea9cf3ec7772
hwangy1 <yuna.hwang@hva.nl>
2026-03-17 15:13:48 +0900

    [FEAT] CORS 설정

---
+++ backend/src/main/java/com/animalfarm/backend/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.animalfarm.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+			.allowedOrigins("http://localhost:5173") // Vite
+			.allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
+}
