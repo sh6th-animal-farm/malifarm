986733b01973711fcd269d4f7cfc98dc1db7d0ee
hwangy1 <yuna.hwang@hva.nl>
2026-03-22 12:25:09 +0900

    [FEAT] 토큰거래소 스프링부트로 마이그레이션

---
+++ backend/src/main/java/com/animalfarm/backend/domain/token/TokenRepository.java
@@ -0,0 +1,27 @@
+package com.animalfarm.backend.domain.token;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.animalfarm.backend.domain.project.dto.TokenLedgerDTO;
+import com.animalfarm.backend.domain.token.dto.TokenDTO;
+
+@Mapper
+public interface TokenRepository {
+
+	TokenDTO selectByProjectId(Long projectId);
+
+	Long selectWalletId(Long userId);
+
+	public abstract void insertTokenLedger(TokenLedgerDTO projectNewTokenDTO);
+
+	void insertTokenLedgerBatch(List<TokenLedgerDTO> tokenLedgerList);
+
+	String selectLastHash();
+
+	void updateDeletedAt(Long tokenId);
+
+	void updateTokenBalance(Long userId, Long tokenId, BigDecimal balanceAfter);
+}
