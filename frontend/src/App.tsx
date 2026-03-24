272d6f61b7e39064451b5ed70d8f0d88f3dbbbad
hwangy1 <yuna.hwang@hva.nl>
2026-03-24 10:52:20 +0900

    [FEAT] 웹페이지에서 사용할 아이콘 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import NotFound from "./pages/Error/NotFound";
 
