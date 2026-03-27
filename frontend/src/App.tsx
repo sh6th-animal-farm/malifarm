90afc616e854cf027bf4b67d9dcda0f6f00765bf
Yanghaji <didwldms0415@gmail.com>
2026-03-27 12:35:44 +0900

    [FEAT] 탄소 리액트 변환

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import NotFound from "./pages/Error/NotFound";
+import CarbonList from "./pages/Carbon/CarbonList";
+import CarbonDetail from "./pages/Carbon/CarbonDetail";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <main className="content-wrapper">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/carbon/list" element={<CarbonList />} />
+          <Route path="/carbon/:id" element={<CarbonDetail />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
