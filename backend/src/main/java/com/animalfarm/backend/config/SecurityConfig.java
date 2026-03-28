85895b4409702a29e0a33119998ec635c42e5266
minsjes <minsjes@minsjes-MacBook-Pro.local>
2026-03-29 03:08:29 +0900

    [FEAT] Security CORS에 PATCH 추가

--- backend/src/main/java/com/animalfarm/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/animalfarm/backend/config/SecurityConfig.java
@@ -113,7 +113,7 @@ public class SecurityConfig {
 		// 프론트엔드 도메인 허용
 		configuration.setAllowedOrigins(
 			List.of("https://mlfarm.3jun.store", "http://localhost:9999", "http://localhost:5173"));
-		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
 
