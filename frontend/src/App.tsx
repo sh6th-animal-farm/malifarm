ae884014653749486cc6220c68a3f3c70bbc1963
hwangy1 <yuna.hwang@hva.nl>
2026-03-29 13:16:19 +0900

    [FEAT] 토큰거래소 상세페이지 거래 카드 변환

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import "./styles/index.css";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import CarbonList from "./pages/Carbon/CarbonList";
-import CarbonDetail from "./pages/Carbon/CarbonDetail";
-import Login from "./pages/Auth/Login";
-import ProjectDetail from "./pages/project/ProjectDetail";
-import TokenList from "./pages/Token/TokenList";
-import Home from "./pages/Home";
-import NotFound from "./pages/Error/NotFound";
+import { Routes, Route } from 'react-router-dom';
+import '@/styles/index.css';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+import Home from '@/pages/Home';
+import Login from '@/pages/Auth/Login';
+import CarbonList from '@/pages/Carbon/CarbonList';
+import CarbonDetail from '@/pages/Carbon/CarbonDetail';
+import ProjectDetail from '@/pages/project/ProjectDetail';
+import TokenList from '@/pages/Token/TokenList';
+import TokenDetail from '@/pages/Token/TokenDetail';
+import NotFound from '@/pages/Error/NotFound';
 
 function App() {
   return (
@@ -18,11 +19,12 @@ function App() {
       <main className="content-wrapper">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
           <Route path="/carbon/list" element={<CarbonList />} />
           <Route path="/carbon/:id" element={<CarbonDetail />} />
-          <Route path="/auth/login" element={<Login />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
           <Route path="/token" element={<TokenList />} />
+          <Route path="/token/:id" element={<TokenDetail />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
--- frontend/src/api/tokenApi.ts
+++ frontend/src/api/tokenApi.ts
@@ -1,9 +1,27 @@
-import type { Token, TokenSummaryInfo } from "../types/tokenType";
-import apiClient from "./apiClient";
+import type {
+  CandleStick,
+  Order,
+  Token,
+  TokenPendingItem,
+  TokenSummaryInfo,
+} from '@/types/tokenType';
+import apiClient from '@/api/apiClient';
 
 export const tokenApi = {
-    getTokenInfo: (tokenId: number) => apiClient.get<Token>(`/api/token/${tokenId}`),
-    getTokenList: () => apiClient.get<Token[]>("/api/home/token"),
-    getOhlcv: (tokenId: number) => apiClient.get<TokenSummaryInfo>(`/api/token/ohlcv/${tokenId}`),
-    getCandles: (tokenId: number, unit: number = 1) => apiClient.get(`/api/market/candles/${tokenId}?unit=${unit}`),
-}
+  getTokenInfo: (tokenId: number) =>
+    apiClient.get<Token>(`/api/token/${tokenId}`),
+  getTokenList: () => apiClient.get<Token[]>('/api/home/token'),
+  getOhlcv: (tokenId: number) =>
+    apiClient.get<TokenSummaryInfo>(`/api/token/ohlcv/${tokenId}`),
+  getCandles: (tokenId: number, unit: number = 1) =>
+    apiClient.get<CandleStick[]>(`/api/market/candles/${tokenId}?unit=${unit}`),
+  getCashBalance: () => apiClient.get<string>('/api/account/balance'),
+  getTokenBalance: (tokenId: number) =>
+    apiClient.get<string>(`/api/account/balance/${tokenId}`),
+  createOrder: (tokenId: number, order: Order) =>
+    apiClient.post<Order>(`/api/token/order/${tokenId}`, order),
+  cancelOrder: (tokenId: number, orderId: number) =>
+    apiClient.post<void>(`/api/token/order-cancel/${tokenId}/${orderId}`),
+  getPendingList: (tokenId: number) =>
+    apiClient.get<TokenPendingItem[]>(`/api/token/pending/${tokenId}`),
+};
