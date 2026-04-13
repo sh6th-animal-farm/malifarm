396a89f422d1d6b2eeaff5f4b82e59da2a0a67cf
Minje Kang <kangminje00@gmail.com>
2026-04-13 11:38:34 +0900

    Update CarbonSnapshotEventDTO.java

--- backend/src/main/java/com/animalfarm/backend/domain/carbon/dto/CarbonSnapshotEventDTO.java
+++ backend/src/main/java/com/animalfarm/backend/domain/carbon/dto/CarbonSnapshotEventDTO.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CarbonSnapshotEventDTO {
 	private Long snapshotId;
-	private String seasonYm;				// 시즌 식별값 Year mont: 2025 11
+	private String seasonYm;				// 시즌 식별값 Year month: 2025 11
 	private LocalDateTime plannedAt;		// 스냅샷 예약 시간 (랜덤)
 	private LocalDateTime snapshotAt;		// 실제 스냅샷 완료 시간
 	private String status;					// 스냅샷 완료 유무
