e69920ae6b985acb4e31d4d0650005f431f28dbc
hwangy1 <yuna.hwang@hva.nl>
2026-04-15 16:12:27 +0900

    [FIX] 토큰 거래소 차트 지연  버그 해결

--- frontend/src/pages/Token/TokenDetail.tsx
+++ frontend/src/pages/Token/TokenDetail.tsx
@@ -13,7 +13,7 @@ export default function TokenDetail() {
   const { id } = useParams(); // URL 파라미터에서 토큰 ID 추출
   const navigate = useNavigate();
   const isFixed = useRef(false); // 가격이 고정되었는지 저장
-  const [fixedPrice, setFixedPrice] = useState<number | null>(null);   // 최초 로드 시 시장가로 세팅
+  const [fixedPrice, setFixedPrice] = useState<number | null>(null); // 최초 로드 시 시장가로 세팅
   const [selectedPrice, setSelectedPrice] = useState<number | null>(null); // 사용자가 직접 선택한 가격
 
   // 훅을 통한 데이터 관리 (에러 방지를 위해 기본값 [] 설정)
@@ -26,6 +26,7 @@ export default function TokenDetail() {
   useEffect(() => {
     setFixedPrice(null);
     setSelectedPrice(null);
+    isFixed.current = false;
   }, [id]);
 
   // 최초 1회만 marketPrice를 fixedPrice에 저장
