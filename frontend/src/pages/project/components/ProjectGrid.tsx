32ae768d8b33044ceefe4023760d29bb98578ffa
minsjes <minsjes@minsjes-MacBook-Pro.local>
2026-04-19 15:30:32 +0900

    [FIX] class 수정

--- frontend/src/pages/project/components/ProjectGrid.tsx
+++ frontend/src/pages/project/components/ProjectGrid.tsx
@@ -79,7 +79,7 @@ export default function ProjectGrid({
   }
 
   return (
-    <div className="grid grid-cols-1 min-[768px]:grid-cols-2 min-[1024px]:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {filteredAndFormatted.map((project) => (
         <ProjectCard
           key={project.id}
