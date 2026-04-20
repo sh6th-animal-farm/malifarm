a3ebf991932e9d1570b788e5e3208b89828b2eb0
minsjes <minsjes@minsjes-MacBook-Pro.local>
2026-04-21 03:08:30 +0900

    [CHORE] 브랜치 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -91,7 +91,7 @@ function App() {
 
   return (
     <div className="flex flex-col h-[var(--app-height)] overflow-hidden md:h-auto md:overflow-visible">
-
+      <Header />
       <main className="flex-1 overflow-hidden md:overflow-visible pb-[var(--bottom-tabbar-height)] lg:pt-[var(--spacing-header-height)]">
         <ScrollToTop />
         <Routes>
